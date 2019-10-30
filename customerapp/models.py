7f3a94a7da46f000a091d10d085163d83c15f2fe
Rismaputri <rismaputrisma@gmail.com>
2019-10-30 10:44:33 +0700

    django_migration

--- customerapp/models.py
+++ customerapp/models.py
@@ -1,8 +1,19 @@
 from django.db import models
 
+JK = (('L','Laki-Laki'),
+      ('P','Perempuan'),)
+OCCUPATION = (('N','NON-FIX'),
+              ('F','FIX'),)
+
+
 # Create your models here.
 class Customer(models.Model):
     nama = models.fields.CharField(max_length=255)
+    jk = models.fields.CharField(max_length=1, choices=JK, default='L')
+    gaji = models.fields.FloatField()
+    existing = models.fields.BooleanField()
+    occupation = models.fields.CharField(max_length=5, choices=OCCUPATION, default='F')
+    alamat = models.fields.TextField()
 
     def __str__(self):
         return self.nama
