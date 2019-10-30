fbac61d4ff1449e4b5d2c654eba4d59234c7d869
Rismaputri <rismaputrisma@gmail.com>
2019-10-30 13:26:31 +0700

    django_readcsv

--- customerapp/views.py
+++ customerapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+import csv
+from django.http import HttpResponse
+from customerapp.models import  Customer
+
+def customer_csv(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename=customerapp.csv'
+    writer = csv.writer(response)
+    customer = Customer.objects.all()
+
+    writer.writerow(['Nama','Gaji','Jenis Kelamin','Existing','Occupation','Alamat','Kendaraan'])
+    for customer in customer:
+        writer.writerow([customer.nama, customer.gaji, customer.jk, customer.existing , customer.occupation,
+                         customer.alamat, customer.kendaraan])
+
+    return response
